9aca689df3e1376e1e4765cac3f62757ec1ec7d8
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-03-21 17:47:34 -0500

    revamp auth page

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,16 +1,46 @@
-import {Title, Text, Anchor, Paper, Group, TextInput, Center, rem, Box, Button, Container, Loader} from "@mantine/core";
-import {IconArrowLeft} from "@tabler/icons-react";
+import {
+  Title,
+  Text,
+  Anchor,
+  Paper,
+  TextInput,
+  Button,
+  PasswordInput, Checkbox, Image, Box, LoadingOverlay
+} from "@mantine/core";
 import classes from "~/styles/routes/auth.module.css";
 import {useNavigate, useOutletContext} from "@remix-run/react";
 import {Session, SupabaseClient} from "@supabase/auth-helpers-remix";
 import {Database} from "~/types/database.types";
 import {useEffect, useState} from "react";
+import {useForm} from "@mantine/form";
 
 function Auth() {
   const navigate = useNavigate();
+  const [register, setRegister] = useState<boolean>(false);
   const [email, setEmail] = useState<string>("");
   const [disabled, setDisabled] = useState<boolean>(false);
   const {supabase, session} = useOutletContext<{ supabase: SupabaseClient<Database>, session: Session | null }>();
+  const loginForm = useForm({
+    initialValues: {
+      email: "",
+    },
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Email invalido'),
+    }
+  })
+
+  const registerForm = useForm({
+    initialValues: {
+      name: "",
+      lastName: "",
+      email: "",
+    },
+    validate: {
+      name: (value) => (value.length > 0 ? null : 'El nombre no puede estar vacio'),
+      lastName: (value) => (value.length > 0 ? null : 'El apellido no puede estar vacio'),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Email invalido'),
+    }
+  })
 
   async function sendLink() {
     setDisabled(true);
@@ -29,40 +59,72 @@ function Auth() {
   }, []);
 
   return (
-    <div style={{
-      width: "100vw",
-      height: "100vh",
-    }}>
-      <Center style={{
-        height: "100%"
-      }}>
-        <Container>
-          <Title className={classes.title} ta="center">
-            Bienvenido, Identificate
+    <div className={classes.wrapper}>
+      <Paper className={classes.form} radius={0} p={30}>
+        <Box pos="relative">
+          <LoadingOverlay visible={disabled} zIndex={1000} overlayProps={{radius: "sm", blur: 2}}/>
+          <Image src={"/wellfit-bottom-text.svg"} h={200} fit={"contain"}/>
+          <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
+            Bienvenido a WellFit
           </Title>
-          <Text c="dimmed" fz="sm" ta="center">
-            Ingrese su correo para recibir un link de inicio de sesión
-          </Text>
 
-          <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
-            <TextInput label="Tu correo" placeholder="yo@gmail.com" required value={email}
-                       onChange={(e) => setEmail(e.target.value)}/>
-            <Group justify="space-between" mt="lg" className={classes.controls}>
-              <Anchor href={"https://wellfitclinic.com"} c="dimmed" size="sm" className={classes.control}>
-                <Center inline>
-                  <IconArrowLeft style={{width: rem(12), height: rem(12)}} stroke={1.5}/>
-                  <Box ml={5}>Volver a la pagina principal</Box>
-                </Center>
-              </Anchor>
-              <Button disabled={disabled} className={classes.control} onClick={sendLink}>
-                {disabled ? <Loader/> : "Enviar Enlace"}
-              </Button>
-            </Group>
-          </Paper>
-        </Container>
-      </Center>
+          {!register &&
+              <>
+
+                  <form onSubmit={loginForm.onSubmit(values => console.log(values))}>
+                      <TextInput label="Correo Electronico"
+                                 placeholder="hola@gmail.com" size="md" {...loginForm.getInputProps("email")}/>
+                      <Button fullWidth mt="xl" size="md" type={"submit"}>
+                          Iniciar Sesion
+                      </Button>
+                  </form>
+
+                  <Text ta="center" mt="md">
+                      Ya tienes cuenta?{' '}
+                      <Anchor<'a'> href="#" fw={700} onClick={(event) => {
+                        event.preventDefault()
+                        setRegister((prev) => !prev)
+                      }}>
+                          Registrate
+                      </Anchor>
+                  </Text>
+              </>}
+          {register &&
+              <>
+                  <form onSubmit={(values) => console.log(values)}>
+                      <TextInput required type={"text"}
+                                 label="Primer Nombre"
+                                 placeholder="Tu nombre" size="md"
+                                 {...registerForm.getInputProps("name")}/>
+                      <TextInput required type={"text"}
+                                 label="Primer Apellido"
+                                 placeholder="Tu apellido" size="md"
+                                 mt="md"  {...registerForm.getInputProps("lastName")}/>
+                      <TextInput required type={"email"}
+                                 label="Correo Electronico"
+                                 placeholder="hola@gmail.com" size="md"
+                                 mt="md" {...registerForm.getInputProps("email")}/>
+                      <Button fullWidth mt="xl" size="md" type={"submit"}>
+                          Registrarme
+                      </Button>
+                  </form>
+
+                  <Text ta="center" mt="md">
+                      Ya tienes cuenta?{' '}
+                      <Anchor<'a'> href="#" fw={700} onClick={(event) => {
+                        event.preventDefault()
+                        setRegister((prev) => !prev)
+                      }}>
+                          Inicia Sesion
+                      </Anchor>
+                  </Text>
+              </>
+          }
+        </Box>
+      </Paper>
     </div>
   )
 }
+
 
 export default Auth;
